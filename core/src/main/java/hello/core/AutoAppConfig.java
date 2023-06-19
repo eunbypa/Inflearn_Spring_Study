16d2f1075cca6f2f735ede17a3922504fcf5b9db
CHOEUNBY <eunbypa@gmail.com>
2023-06-19 23:01:35 +0900

    0619 스프링 기본편 섹션 6

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,22 @@
+package hello.core; // default base 패키지
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
