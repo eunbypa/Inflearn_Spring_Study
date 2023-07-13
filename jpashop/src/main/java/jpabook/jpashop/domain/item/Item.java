2bb3ade6f689a4cd7a99cee9fd4559ffb45ab460
eunbypa <harunz@naver.com>
2023-07-13 23:06:41 +0900

    0713 스프링 JPA 활용 1 섹션 3 ~ 섹션 6

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 한 테이블만 생성
@@ -18,5 +22,27 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    //setter 대신 비즈니스 메서드를 사용하는 것이 객체지향 적인 방향
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+    }
 
 }
---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 조회에 대하여 더티체킹 방지
+@RequiredArgsConstructor
+public class MemberService {
+
+    // final을 적용하면 생성자 주입을 안했을 때 컴파일 시 에러를 확인 가능
+    private final MemberRepository memberRepository;
+
+//    @Autowired
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
